dfea21a35d667969b61577c62bdffc0238a0e553
Caoimhe Clarke <caoimhepower@gmail.com>
2018-08-01 13:48:52 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -39,11 +39,27 @@ console.log("Sorry, try again.");
 };
 
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+		this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch ();
-}
+};
+};
 
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	};
+};
+
+/*
 console.log("User flipped " + cards[cardId].rank)
 cardsInPlay.push(cards[cardId].rank);
 
@@ -52,9 +68,7 @@ cardsInPlay.push(cards[cardId].cardImage);
 
 console.log("User flipped " + cards[cardId].suit)
 cardsInPlay.push(cards[cardId].suit);
-};
 
+*/
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
